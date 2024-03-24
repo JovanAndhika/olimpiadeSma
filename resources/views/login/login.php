4fcda4cb3b8bf8f814314a05790d937f012a13dc
Dohazo <pootisspy931@gmail.com>
2024-03-24 14:22:40 +0700

    dummy
    buat login baru di views

---
+++ resources/views/login/login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="login.css">
+    <!-- Bootstrap icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+
+
+    
+    </style>
+        <title>LOGIN BOM</title>
+</head>
+<body>
+    <div class="box rounded-top border border-blue m-0">
+        <form action="">
+            <div class="row center">
+                <div class="col-8">
+                    <H2 class="">LOGIN</H2>
+                </div>
+                <div class="col-8">
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username">
+                    <i class="bi bi-person-circle"></i>
+                    <span></span>
+                </div>
+                <div class="col-8">
+                <label for="password">Password</label>
+                    <input type="password" name="password" id="password">
+                    <span><i class="bi bi-eye-slash"></i></span>
+
+                </div>
+                <div class="col-8">
+                    <button type="submit">Login</button>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </form>
+    </div>
+</body>
+</html>
